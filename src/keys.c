25d2c93bdef65efbe209e5e1ca92d50845540b69
jmehmy <jaskaran.prince1@gmail.com>
2025-03-27 19:26:42 +0000

    upload keys.c

---
+++ src/keys.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   keys.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmehmy <jmehmy@student.42lisboa.com>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/27 18:23:03 by jmehmy            #+#    #+#             */
+/*   Updated: 2025/03/27 19:20:37 by jmehmy           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/so_long.h"
+
+int handle_key_press(int key, t_window *mlx)
+{
+    if (key == UP || key == LEFT || key == RIGHT  || key == DOWN)
+    {
+        move_player(mlx, key);
+        create_map(mlx, (*mlx).map);
+    }
+    else if (key == ESC)
+        exit_game(mlx);
+    return (0);
+}
