7909d9571e4dc2b5722b6828c0720b02749474ca
jmehmy <jmehmy@student.42lisboa.com>
2025-04-01 18:04:39 +0100

    player start moving now

--- src/graphics.c
+++ src/graphics.c
@@ -6,7 +6,7 @@
 /*   By: jmehmy <jmehmy@student.42lisboa.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/01 12:49:30 by jmehmy            #+#    #+#             */
-/*   Updated: 2025/04/01 17:28:47 by jmehmy           ###   ########.fr       */
+/*   Updated: 2025/04/01 18:03:37 by jmehmy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,43 +43,50 @@ static void	movement(t_map *map, int x_move, int y_move)
 	static int	items_count = 0;
 	static int	count = 0;
 
+	if (map->split_map[map->player_y + y_move][map->player_x + x_move] == '1')
+		return ;
+	map->split_map[map->player_y][map->player_x] = '0';
 	map->player_y += y_move;
 	map->player_x += x_move;
-	mlx_destroy_image(map->m_pack->mlx, map->m_pack->player);
-	image_2_map(map, map->m_pack->player, map->player_x, map->player_y);
+	map->split_map[map->player_y][map->player_x] = 'P';
+	
+	image_2_map (map, map->m_pack->floor, map->player_x - x_move, map->player_y - y_move);
+	image_2_map (map, map->m_pack->player, map->player_x, map->player_y);
+	
 	ft_putstr_fd("Movements: ", 1);
 	ft_putnbr_fd(++count, 1);
+	write(1, "\n", 1);
 	if (map->split_map[map->player_y][map->player_x] == 'C')
 		item_collected(map, &items_count);
-	else if (map->split_map[map->player_y][map->player_x] == 'E'
+	if (map->split_map[map->player_y][map->player_x] == 'E'
 		&& items_count == map->items)
 	{
 		//(void)write(1, "\033[0;33m", 7);
 		ft_putstr_fd("Well done !", 1);
 		mlx_destroy_window(map->m_pack->mlx, map->m_pack->win);
 		//(void)write(1, "\033[0m", 5);
-		return ;
+		exit(0) ;
 	}
 }
 
-static int	key_hook(int keycode, void *param)
+static int	key_hook(int keycode, t_map *map)
 {
-	t_map	*map;
-
-	map = (t_map *)param;
+	if(!map)
+		return (0);
+		
 	if (keycode == 65307)
+	{ 
+		ft_putstr_fd("Game closed.\n", 1);
 		mlx_destroy_window(map->m_pack->mlx, map->m_pack->win);
-	if (keycode == 119 && map->split_map[map->player_y
-		- 1][map->player_x] != '1')
+		exit(0);
+	}
+	if (keycode == 119 )
 		movement(map, 0, -1);
-	else if (keycode == 100 && map->split_map[map->player_y][map->player_x
-		+ 1] != '1')
+	else if (keycode == 100 )
 		movement(map, 1, 0);
-	else if (keycode == 115 && map->split_map[map->player_y
-		+ 1][map->player_x] != '1')
+	else if (keycode == 115)
 		movement(map, 0, 1);
-	else if (keycode == 97 && map->split_map[map->player_y][map->player_x
-		- 1] != '1')
+	else if (keycode == 97 )
 		movement(map, -1, 0);
 	return (0);
 }
@@ -133,7 +140,7 @@ void	graphics(t_map *map)
 	map->m_pack->exit_open = set_image(map, OPENED);
     
 	render_map(map, 0, 0);
-	mlx_key_hook(map->m_pack->win, &key_hook, map);
+	mlx_hook(map->m_pack->win, 2, 1L << 0,  key_hook, map);
     mlx_hook(map->m_pack->win, 17, 0, (void *)clean_exit, map);
 	mlx_loop(map->m_pack->mlx);
 }
